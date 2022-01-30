26a25066674d7c67e6e08c0f20d1c9bbd8775a7a
Ramin Imani <93437949+raminimani@users.noreply.github.com>
2022-01-29 21:40:41 -0500

    Solar System Basic Specification added, Some features are still under developement

---
+++ src/Class5/Main.java
@@ -0,0 +1,11 @@
+package Class5;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SolarSystem milkway=new SolarSystem();
+
+    }
+
+
+}
---
+++ src/Class5/SolarSystem.java
@@ -0,0 +1,9 @@
+package Class5;
+
+public class SolarSystem {
+
+    int Sun;
+    int Moon;
+    int Planets;
+
+}
